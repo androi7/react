8a739cdc630fb10eb54563459ef9c6b41e73008b
androi <gabriel.hilti@gmail.com>
2019-01-30 18:49:20 +0100

    server request done

--- src/components/Fetch.js
+++ src/components/Fetch.js
@@ -1,12 +1,11 @@
-import React from 'react';
-//import 'babel-polyfill';
+import 'babel-polyfill';
 import 'isomorphic-fetch';
 
 const URL = 'https://www.reddit.com/r/wallpapers.json';
 
 export function fetchJson() {
 	return fetch(URL)
-		.then(response => response)
+		.then(response => response.text())
 		.catch(err => {
 			console.error('parsing failed', err);
 		});
