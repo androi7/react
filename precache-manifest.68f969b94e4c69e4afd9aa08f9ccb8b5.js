187c751874476f91cea800033f25b6503b146892
androi <gabriel.hilti@gmail.com>
2019-01-30 18:25:22 +0100

    Updates

---
+++ precache-manifest.68f969b94e4c69e4afd9aa08f9ccb8b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf2515a31126aef78a60",
+    "url": "/react/static/js/runtime~main.bf2515a3.js"
+  },
+  {
+    "revision": "6886d4b2c9d413488099",
+    "url": "/react/static/js/main.6886d4b2.chunk.js"
+  },
+  {
+    "revision": "4aad9ae53b9f2424817a",
+    "url": "/react/static/js/1.4aad9ae5.chunk.js"
+  },
+  {
+    "revision": "6886d4b2c9d413488099",
+    "url": "/react/static/css/main.88da1adb.chunk.css"
+  },
+  {
+    "revision": "c3a059febcbb87509b9b868c5501602e",
+    "url": "/react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react/precache-manifest.a2a2cb8c0235833d51b00e10978ec86e.js"
+  "/react/precache-manifest.68f969b94e4c69e4afd9aa08f9ccb8b5.js"
 );
 
 workbox.clientsClaim();
