1ed6857485489de7a5a80f114722ac75f5f4ece6
androi <gabriel.hilti@gmail.com>
2019-01-30 16:36:55 +0100

    Updates

---
+++ precache-manifest.a2a2cb8c0235833d51b00e10978ec86e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bf2515a31126aef78a60",
+    "url": "/react/static/js/runtime~main.bf2515a3.js"
+  },
+  {
+    "revision": "3d878e467ea489216e0a",
+    "url": "/react/static/js/main.3d878e46.chunk.js"
+  },
+  {
+    "revision": "796afacf943bb20eb442",
+    "url": "/react/static/js/1.796afacf.chunk.js"
+  },
+  {
+    "revision": "3d878e467ea489216e0a",
+    "url": "/react/static/css/main.4f5dd001.chunk.css"
+  },
+  {
+    "revision": "d4fec25e30a359f75c2a81484e1721d3",
+    "url": "/react/index.html"
+  }
+];
